217e5caec4137faf1609acc4a8711c2095b83bdf
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-23 16:47:09 -0500

    created a method in Contact class in order to add a contact to the .txt file. also began conditional for user input in main menu.

--- src/Contact.java
+++ src/Contact.java
@@ -1,9 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 
 public class Contact {
@@ -17,6 +17,84 @@ public class Contact {
         for (String line : linesInTheFile) {
             System.out.println(line);
         }
-//
+    }
+
+    public void defaultContacts() {
+        // Creating Path to .txt file:
+
+        Path pathToOurFile = Paths.get("src", "contacts.txt");
+
+        try {
+            if (Files.notExists(pathToOurFile)) {
+                Files.createFile(pathToOurFile);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        // Setting Default Contacts:
+
+        // Below code will create our HashMap and will add the Key Value pairs from the HashMap to the contacts.txt. After the enhanced-for loop, the names and phone numbers are printed:
+
+        HashMap<String, String> contacts = new HashMap<>();
+        contacts.put("Shelby ", "(605)475-6958");
+        contacts.put("Joe", "(212)479-7990");
+        contacts.put("Kyle", "(470)651-5050");
+        contacts.put("Chris", "(212)479-7990");
+        contacts.put("Sergio", "(880)651-5050");
+
+        System.out.println("Name | Phone Number");
+        System.out.println("-------------------");
+
+        List<String> listOfContacts = new ArrayList<>();
+
+        for (Map.Entry<String, String> contact : contacts.entrySet()) {
+            String key = contact.getKey();
+            Object value = contact.getValue();
+
+            String concatFormat = key + " | " + value;
+
+            listOfContacts.add(concatFormat);
+
+            try {
+                Files.write(pathToOurFile, listOfContacts);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+        // Instantiating Contact class:
+
+        Contact contact = new Contact();
+
+        // Using below method from Contact class. Will read and print .txt file:
+
+        contact.readFileAndOutput(pathToOurFile);
+    }
+
+    public void addContact() {
+        Path pathToOurFile = Paths.get("src", "contacts.txt");
+
+        Scanner nameScanner = new Scanner(System.in);
+        Scanner phoneNumberScanner = new Scanner(System.in);
+
+        List<String> newAdditions = new ArrayList<>();
+
+        // Below will add a contact to the .txt file. Will move this to a method for better use:
+
+        System.out.println("Please enter the name of the contact: ");
+        String userName = nameScanner.next();
+
+        System.out.println("Please enter the phone number of the contact: ");
+        String userPhoneNumber = phoneNumberScanner.next();
+
+        String concatFormat = userName + " | " + userPhoneNumber;
+
+        newAdditions.add(concatFormat);
+        try {
+            Files.write(pathToOurFile, newAdditions, StandardOpenOption.APPEND);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
--- src/ContactApp.java
+++ src/ContactApp.java
@@ -2,71 +2,49 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class ContactApp {
     
     public static void main(String[] args) {
+        Path pathToOurFile = Paths.get("src", "contacts.txt");
 
-        // Creating Path to .txt file:
+        // Instantiating Contact object:
 
-    Path pathToOurFile = Paths.get("src", "contacts.txt");
+        Contact contact = new Contact();
 
-        try {
-            if (Files.notExists(pathToOurFile)) {
-                Files.createFile(pathToOurFile);
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        // Using method from Contact class:
+
+        contact.defaultContacts();
+
+        // Main Menu:
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("%n%n1. View contacts.%n");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+        // Conditionals required for User Input:
+
+        int userInput = scanner.nextInt();
+
+        if (userInput == 1) {
+            contact.readFileAndOutput(pathToOurFile);
+        } else if (userInput == 2) {
+            contact.addContact();
+        } else if (userInput == 3) {
+
+        } else if (userInput == 4) {
+
+        } else if (userInput == 5) {
+
+        } else {
+            System.out.println("I'm sorry, that is not a valid input.");
         }
-
-        // Setting Default Contacts:
-
-        // Below code will create our HashMap and will add the Key Value pairs from the HashMap to the contacts.txt. After the enhanced-for loop, the names and phone numbers are printed:
-
-    HashMap<String, String> contacts = new HashMap<>();
-        contacts.put("Shelby ", "(605)475-6958");
-        contacts.put("Joe", "(212)479-7990");
-        contacts.put("Kyle", "(470)651-5050");
-        contacts.put("Chris", "(212)479-7990");
-        contacts.put("Sergio", "(880)651-5050");
-
-        System.out.println("Name | Phone Number");
-        System.out.println("-------------------");
-
-        List<String> listOfContacts = new ArrayList<>();
-
-        for (Map.Entry<String, String> contact : contacts.entrySet()) {
-            String key = contact.getKey();
-            Object value = contact.getValue();
-
-            String concatFormat = key + " | " + value;
-
-            listOfContacts.add(concatFormat);
-
-            try {
-                Files.write(pathToOurFile, listOfContacts);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-
-        // Instantiating Contact class:
-
-    Contact contact = new Contact();
-
-        // Using below method from Contact class. Will read and print .txt file:
-
-    contact.readFileAndOutput(pathToOurFile);
-
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.printf("%n%n1. View contacts.%n");
-    System.out.println("2. Add a new contact.");
-    System.out.println("3. Search a contact by name.");
-    System.out.println("4. Delete an existing contact.");
-    System.out.println("5. Exit.");
-    System.out.println("Enter an option (1, 2, 3, 4 or 5):");
-
     }
 }
